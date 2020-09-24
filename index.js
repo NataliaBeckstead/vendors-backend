192b2b267879edcc7526a7c5397d785598d9d62a
Natalia Beckstead <toosmiravilla@gmail.com>
2020-09-23 22:29:30 -0600

    initial get request and gitignore

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.send('hello')
+});
+
 const port = 8000;
 
-server.listen(port, () => console.log(`\n == API on port ${port} == \n`))
+server.listen(port, () => console.log(`\n == API running on port ${port} == \n`))
